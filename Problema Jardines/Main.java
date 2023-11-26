65ca4b16ed87a308803474fe84f138d953272888
Gsus329 <105911027+Gsus329@users.noreply.github.com>
2023-11-26 21:20:39 +0100

    Add files via upload

---
+++ Problema Jardines/Main.java
@@ -0,0 +1,16 @@
+package jardin;
+
+public class Main {
+
+	public static void main(String[] args) {
+        RecursoJardin jardin = new RecursoJardin();
+        //crea un objeto RecursoJardín
+        for (int i = 1; i <= 10; i++) {
+            (new EntraJardin("Entra" + i, jardin)).start();
+        }//entrada de 10 hilos al jardín
+        for (int i = 1; i <= 15; i++) {
+            (new SaleJardin("Sale" + i, jardin)).start();
+        }//salida de 15 hilos al jardín
+    }
+
+}
